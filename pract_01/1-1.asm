d253e4db0cacbc9c7134e645f7a4caa2d8a7bc11
Anton Matveev <as_matveev1@student.mpgu.edu>
2022-09-13 16:03:27 +0300

    Практическая работа № 1 (13 сентября 2022 года)

---
+++ pract_01/1-1.asm
@@ -0,0 +1,18 @@
+.MODEL SMALL                ; код вмещается в один сегмент памяти
+.STACK 100h                 ; на выполнение программы выделяется 256 бит
+
+.DATA                       ; сегмент данных
+	MSG db 'Hello, World!$' ; db - define byte, dw - define word (2 byte), dd - double word (4 byte), dq - quad word (8 byte)
+
+.CODE                       ; сегмент кода (все команды указываются в нём)
+begin:                      ; метка входа в программу
+    mov AX, @data           ; запись адреса сегмента данных
+    mov DS, AX
+	
+    mov AH, 9h              ; вывод сообщения в консоль
+    mov DX, offset MSG
+    int 21h
+	
+    mov AX, 4C00h           ; выход из программы
+    int 21h
+END begin                   ; команда завершения программы (END в программе должен быть ровно один)
---
+++ pract_01/1-2.asm
@@ -0,0 +1,26 @@
+.MODEL SMALL                ; код вмещается в один сегмент памяти
+.STACK 100h                 ; на выполнение программы выделяется 256 бит
+
+.DATA                       ; сегмент данных
+	MSG1 db 'Hello, world!$' ; первая строка
+    MSG2 db 'How are you?$' ; вторая строка
+    NXTLN db 10, 13, '$'    ; перенос строки
+
+.CODE                       ; сегмент кода
+begin:                      ; метка входа в программу
+    ; запись адреса сегмента данных
+    mov AX, @data
+    mov DS, AX
+	
+    ; вывод сообщений в консоль
+    mov AH, 9h
+    mov DX, offset MSG1
+    int 21h
+    mov DX, offset NXTLN
+    int 21h
+	mov DX, offset MSG2
+    int 21h
+
+    mov AX, 4C00h           ; выход из программы
+    int 21h
+END begin                   ; завершение программы
